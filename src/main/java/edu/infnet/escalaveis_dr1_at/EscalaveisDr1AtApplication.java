ffa68dbaa0334c048f90bd28bf0d0473aa299b4a
jvcmtr <jvictorcmtr@gmail.com>
2025-09-19 00:26:43 -0300

    init project using Spring Initialzr

---
+++ src/main/java/edu/infnet/escalaveis_dr1_at/EscalaveisDr1AtApplication.java
@@ -0,0 +1,13 @@
+package edu.infnet.escalaveis_dr1_at;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaveisDr1AtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaveisDr1AtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/infnet/escalaveis_dr1_at/EscalaveisDr1AtApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infnet.escalaveis_dr1_at;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaveisDr1AtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
